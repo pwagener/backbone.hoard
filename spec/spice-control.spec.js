7d7087b8fbba6ae35077f142df3e8f7351380dd4
Christian Maher <maher.cs@gmail.com>
2014-08-31 15:36:21 -0400

    Populating the cache on a cache miss and successful request

--- spec/spice-control.spec.js
+++ spec/spice-control.spec.js
@@ -1,15 +1,19 @@
 'use strict';
 
 var Backbone = require('backbone');
+var Spice = require('./support/spice');
 var SpiceControl = require('./support/spice-control');
 
 describe("SpiceControl", function () {
+  beforeEach(function () {
+    this.modelUrl = 'theUrl';
+    this.Model = Backbone.Model.extend({ url: this.modelUrl });
+  });
+
   describe("construction", function () {
     beforeEach(function () {
-      this.store = new Backbone.Model();
       this.backend = this.sinon.stub();
       this.options = {
-        store: this.store,
         backend: this.backend
       };
       this.initializeSpy = this.sinon.spy(SpiceControl.prototype, 'initialize');
@@ -18,10 +22,6 @@ describe("SpiceControl", function () {
 
     it("should create a SpiceControl", function () {
       expect(this.spiceControl).to.be.instanceOf(SpiceControl);
-    });
-
-    it("should assign the provided store", function () {
-      expect(this.spiceControl.store).to.equal(this.store);
     });
 
     it("should assign the provided backend", function () {
@@ -37,43 +37,109 @@ describe("SpiceControl", function () {
 
   describe("generateKey", function () {
     beforeEach(function () {
-      this.Model = Backbone.Model.extend({
-        url: 'theUrl'
-      });
-
-      this.spiceControl = new SpiceControl({ store: new this.Model() });
-      this.key = this.spiceControl.generateKey();
+      this.model = new this.Model();
+      this.spiceControl = new SpiceControl();
+      this.key = this.spiceControl.generateKey(this.model);
     });
 
     it("should return the result of the url, by default", function () {
-      expect(this.key).to.equal('theUrl');
+      expect(this.key).to.equal(this.modelUrl);
     });
   });
 
   describe("invalidate", function () {
     beforeEach(function () {
+      var spec = this;
       this.Model = Backbone.Model.extend({
         url: function () {
-          return 'theUrl';
+          return spec.modelUrl;
         }
       });
+      this.model = new this.Model();
 
-      this.spiceControl = new SpiceControl({ store: new this.Model() });
+      this.spiceControl = new SpiceControl();
       this.sinon.spy(this.spiceControl, 'generateKey');
       this.sinon.stub(this.localStorage, 'removeItem');
 
       this.method = 'read';
-      this.spiceControl.invalidate(this.method);
+      this.spiceControl.invalidate(this.model, this.method);
     });
 
     it("calls generateKey with the provided method", function () {
       expect(this.spiceControl.generateKey).to.have.been.calledOnce
-        .and.calledWith(this.method);
+        .and.calledWith(this.model, this.method);
     });
 
     it("removes the key returned from generateKey from the backend", function () {
       expect(this.localStorage.removeItem).to.have.been.calledOnce
-        .and.calledWith('theUrl');
+        .and.calledWith(this.modelUrl);
+    });
+  });
+
+  describe("sync", function () {
+    beforeEach(function () {
+      var spec = this;
+      this.model = new this.Model();
+      this.serverResponse = { myResponse: true };
+      this.ajax = Spice.deferred();
+
+      this.syncResponse = this.ajax.promise.then(function () {
+        spec.ajaxOptions.success(spec.serverResponse);
+      });
+
+      this.sinon.stub(Backbone, 'ajax', function (options) {
+        spec.ajaxOptions = options;
+        return spec.syncResponse;
+      });
+
+      this.options = {};
+      this.placeholder = { placeholder: true };
+      this.spiceControl = new SpiceControl({ store: this.model });
+      this.sinon.spy(this.spiceControl, 'onRead');
+      this.sinon.spy(this.model, 'sync');
+    });
+
+    describe("method: read", function () {
+      it("calls onRead with the model and the options", function () {
+        this.spiceControl.sync('read', this.model, this.options);
+        expect(this.spiceControl.onRead).to.have.been.calledOnce
+          .and.calledWith(this.model, this.options);
+      });
+
+      it("reads the key from the cache", function () {
+        this.sinon.stub(this.localStorage, 'getItem');
+        this.spiceControl.sync('read', this.model, this.options);
+        expect(this.localStorage.getItem).to.have.been.calledOnce
+          .and.calledWith(this.spiceControl.generateKey(this.model, 'read'));
+      });
+
+      describe("on a cache miss", function () {
+        beforeEach(function () {
+          this.sinon.stub(this.localStorage, 'getItem').returns(null);
+          this.sinon.stub(this.localStorage, 'setItem');
+          this.syncReturn = this.spiceControl.sync('read', this.model, this.options);
+        });
+
+        it("calls the underlying model's sync with the same arguments", function () {
+          expect(this.model.sync).to.have.been.calledOnce
+            .and.calledWith('read', this.model, this.options);
+        });
+
+        it("inserts a placeholder entry for the key", function () {
+          expect(this.localStorage.setItem).to.have.been
+            .calledWith(this.modelUrl, JSON.stringify(this.placeholder));
+        });
+
+        it("writes to the cache when the model's sync resolves", function (done) {
+          var spec = this;
+          this.ajax.resolve();
+          this.syncResponse.then(function () {
+            expect(spec.localStorage.setItem).to.have.been
+              .calledWith(spec.modelUrl, JSON.stringify(spec.serverResponse));
+            done();
+          });
+        });
+      });
     });
   });
 });
--- src/spice-control.js
+++ src/spice-control.js
@@ -4,22 +4,63 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 var SpiceConfig;
 
-var mergeOptions = ['store', 'backend'];
+var mergeOptions = ['backend'];
 
 var SpiceControl = function (options) {
   _.extend(this, _.pick(options || {}, mergeOptions));
   this.initialize.apply(this, arguments);
 };
 
+var methodHandlers = {
+  create: 'onCreate',
+  read: 'onRead',
+  update: 'onUpdate',
+  delete: 'onDestroy'
+};
+
 _.extend(SpiceControl.prototype, {
   initialize: function () {},
 
-  generateKey: function (method) {
-    return _.result(this.store, 'url');
+  generateKey: function (model, method) {
+    return _.result(model, 'url');
   },
 
-  invalidate: function (method) {
-    this.backend.removeItem(this.generateKey(method));
+  invalidate: function (model, method) {
+    this.backend.removeItem(this.generateKey(model, method));
+  },
+
+  sync: function (method, model, options) {
+    var handlerName = methodHandlers[method];
+    return this[handlerName](model, options);
+  },
+
+  onCreate: function (model, options) {
+  },
+
+  onRead: function (model, options) {
+    var key = this.generateKey(model, 'read');
+    var item = this.backend.getItem(key);
+    if (item === null) {
+      this.backend.setItem(key, JSON.stringify({ placeholder: true }));
+      options.success = this.getCacheMissOnSuccess(key, model, options);
+      return model.sync('read', model, options);
+    }
+  },
+
+  getCacheMissOnSuccess: function (key, model, options) {
+    var backend = this.backend;
+    return _.wrap(options.success, function (onSuccess, response) {
+      backend.setItem(key, JSON.stringify(response));
+      if (onSuccess) {
+        onSuccess.apply(this, _.rest(arguments));
+      }
+    });
+  },
+
+  onUpdate: function () {
+  },
+
+  onDestroy: function () {
   }
 });
 
